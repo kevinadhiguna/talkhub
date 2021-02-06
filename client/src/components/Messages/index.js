fd4889e02a047ebd5c8b10085548df65e788f62c
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-02-07 00:45:19 +0700

    refactor: complete Messages component

--- client/src/components/Messages/index.js
+++ client/src/components/Messages/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+import Message from './Message';
+import { StyledMessages } from './styles';
 
 function Messages(props) {
+    const { messages, username } = props;
+    
     return(
-        <></>
+        <StyledMessages>
+            <ScrollToBottom>
+                {
+                    messages.map((message, i) => {
+                        <div key={i}>
+                            <Message
+                                message={message}
+                                username={username}
+                            />
+                        </div>
+                    })
+                }
+            </ScrollToBottom>
+        </StyledMessages>
     );
 }
 
